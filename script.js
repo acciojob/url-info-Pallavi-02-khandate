8e5603ab6989116971bc4552b8224e0c1f54c0f1
Pallavi-02-khandate <pkhandate2002@gmail.com>
2026-02-03 20:56:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ const currentURL = window.location.href;
+
+  // Find length
+  const lengthOfURL = currentURL.length;
+
+  // Show alert
+  alert("The length of the URL is: " + lengthOfURL);
